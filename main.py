7d597390594ca0dae2e2bedb4f5bdce4b0b3684b
immccc <immccc@gmail.com>
2025-08-01 19:01:23 +0200

    Implement functionality on main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-import requests
+import argparse
+import asyncio
 
-def main():
-    print("Hello from pvpc-home!")
+from charger.tariffs import create_tariff
+
+def _parse_args():
+    parser = argparse.ArgumentParser(description="Create a tariff using Ohme API.")
+    parser.add_argument("--name", type=str, help="Name of the tariff to create", default="PVPC")
+    return parser.parse_args()
+    
+def main(args):
+    asyncio.run(create_tariff(args.name))
 
 
 if __name__ == "__main__":
-    main()
+    args = _parse_args()
+    main(args)
